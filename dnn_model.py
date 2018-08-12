ee6aeac8715c1ce38c9f66e4de6c7601b95499d9
alexma <madi171@gmail.com>
2018-08-12 19:20:43 +0800

    dnn model class

--- dnn_model.py
+++ dnn_model.py
@@ -8,19 +8,25 @@ import os
 import numpy as np
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array
-from keras.applications.mobilenetv2 import MobileNetV2
+from sklearn.cross_validation import train_test_split
+
+from squeezenet import SqueezeNet
 from keras.optimizers import Adam
 from keras.utils import to_categorical
 
 
+
 class DNNModel:
-    def __init__(self):
+    def __init__(self, image_path):
         self.IMAGE_SIZE = 64
         self.data = []
         self.labels = []
         self.model = self.build_model()
 
-        self.image_path = "/home/madi/deeplearning/raspberry-pi/datasets"
+        if image_path is not None:
+            self.image_path = image_path
+        else:
+            self.image_path = "/home/madi/deeplearning/raspberry-pi/datasets"
         pass
 
     def gen_training_image_set(self):
@@ -36,9 +42,9 @@ class DNNModel:
             # extract the class label from the image path and update the
             # labels list]
             if "left" in imagePath.split(os.path.sep)[-2]:
-                label = -1
-            elif "right" in imagePath.split(os.path.sep)[-2]:
                 label = 1
+            elif "right" in imagePath.split(os.path.sep)[-2]:
+                label = 2
             else:
                 label = 0
 
@@ -49,8 +55,8 @@ class DNNModel:
         self.labels = np.array(self.labels)
 
     def build_model(self):
-        self.model = MobileNetV2(width=28, height=28, depth=3, classes=2)
-        opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+        self.model = SqueezeNet(include_top=False, weights=None, classes=3)
+        opt = Adam()
         self.model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
         pass
 
@@ -59,8 +65,8 @@ class DNNModel:
         (trainX, testX, trainY, testY) = train_test_split(self.data, self.labels, test_size=0.25, random_state=42)
 
         # convert the labels from integers to vectors
-        trainY = to_categorical(trainY, num_classes=2)
-        testY = to_categorical(testY, num_classes=2)
+        trainY = to_categorical(trainY, num_classes=3)
+        testY = to_categorical(testY, num_classes=3)
 
         self.model.fit(trainX, trainY, batch_size=32, epochs=10, verbose=1)
 
@@ -78,11 +84,11 @@ class DNNModel:
         pass
 
     def save_model(self):
-        self.model.save("mnistmodel.h5")
+        self.model.save("greenball_squeezenet.h5")
         pass
 
     def load_model(self):
-        self.model = load_model("mnistmodel.h5")
+        self.model = load_model("greenball_squeezenet.h5")
         pass
 
 
