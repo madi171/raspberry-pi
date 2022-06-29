3eb3493d0dcc1904b1e84f10d228aa89cc8ab595
adamma <adamma@tencent.com>
2022-06-29 19:17:01 +0800

    update car control agent

--- autocars/src/raspberry_pi/cars_agent.py
+++ autocars/src/raspberry_pi/cars_agent.py
@@ -51,8 +51,10 @@ class Cars:
         '''
         Motor speed define
         '''
-        self.VIECLE_SPEED_HIGH = 80 # high speed
-        self.VIECLE_SPEED_LOW = 40 # low speed
+        self.VIECLE_SPEED_HIGH = 90     # high speed
+        self.VIECLE_SPEED_DEFAULT = 70  # default speed
+        self.VIECLE_SPEED_LOW = 50      # low speed
+        self.VIECLE_TURNING_SPEED = 1   # speed of turning wheel
 
         '''
         Supersonic const define
@@ -69,8 +71,6 @@ class Cars:
         self.IRF_R = 23  # 小车跟随右侧红外
         self.IRF_L = 24  # 小车跟随左侧红外
 
-
-
         GPIO.setwarnings(False)
 
         '''
@@ -83,17 +83,20 @@ class Cars:
         '''
         Motor init to set to default freq and speed
         '''
+        self.cur_left_speed = self.VIECLE_SPEED_DEFAULT
+        self.cur_right_speed = self.VIECLE_SPEED_DEFAULT
+
         GPIO.setup(self.ENA, GPIO.OUT, initial=GPIO.LOW)
-        self.ENA_pwm = GPIO.PWM(self.ENA, 1000)
+        self.ENA_pwm = GPIO.PWM(self.ENA, 1000) # define left engine
         self.ENA_pwm.start(0)
-        self.ENA_pwm.ChangeDutyCycle(100)
+        self.ENA_pwm.ChangeDutyCycle(self.VIECLE_SPEED_DEFAULT)
         GPIO.setup(self.IN1, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.IN2, GPIO.OUT, initial=GPIO.LOW)
 
         GPIO.setup(self.ENB, GPIO.OUT, initial=GPIO.LOW)
-        self.ENB_pwm = GPIO.PWM(self.ENB, 1000)
+        self.ENB_pwm = GPIO.PWM(self.ENB, 1000) # define right engine
         self.ENB_pwm.start(0)
-        self.ENB_pwm.ChangeDutyCycle(100)
+        self.ENB_pwm.ChangeDutyCycle(self.VIECLE_SPEED_DEFAULT)
         GPIO.setup(self.IN3, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.IN4, GPIO.OUT, initial=GPIO.LOW)
 
@@ -145,27 +148,23 @@ class Cars:
 
     def turn_left(self):
         print 'motor_turnleft'
-        GPIO.output(self.ENA, True)
-        GPIO.output(self.ENB, True)
-        GPIO.output(self.IN1, True)
-        GPIO.output(self.IN2, False)
-        GPIO.output(self.IN3, False)
-        GPIO.output(self.IN4, True)
-        GPIO.output(self.LED1, False)  # LED1亮
-        GPIO.output(self.LED2, True)  # LED2灭
-        self.set_speed(self.VIECLE_SPEED_LOW)
+        self.cur_left_speed -= self.VIECLE_TURNING_SPEED
+        self.cur_left_speed = max(self.cur_left_speed, self.VIECLE_SPEED_LOW)
+        self.cur_right_speed += self.VIECLE_TURNING_SPEED
+        self.cur_right_speed = min(self.cur_right_speed, self.VIECLE_SPEED_HIGH)
+        self.ENA_pwm.ChangeDutyCycle(self.cur_left_speed)
+        self.ENB_pwm.ChangeDutyCycle(self.cur_right_speed)
+        self.go_forward()
 
     def turn_right(self):
         print 'motor_turnright'
-        GPIO.output(self.ENA, True)
-        GPIO.output(self.ENB, True)
-        GPIO.output(self.IN1, False)
-        GPIO.output(self.IN2, True)
-        GPIO.output(self.IN3, True)
-        GPIO.output(self.IN4, False)
-        GPIO.output(self.LED1, False)  # LED1亮
-        GPIO.output(self.LED2, True)  # LED2灭
-        self.set_speed(self.VIECLE_SPEED_LOW)
+        self.cur_right_speed -= self.VIECLE_TURNING_SPEED
+        self.cur_right_speed = max(self.cur_right_speed, self.VIECLE_SPEED_LOW)
+        self.cur_left_speed += self.VIECLE_TURNING_SPEED
+        self.cur_left_speed = min(self.cur_left_speed, self.VIECLE_SPEED_HIGH)
+        self.ENA_pwm.ChangeDutyCycle(self.cur_left_speed)
+        self.ENB_pwm.ChangeDutyCycle(self.cur_right_speed)
+        self.go_forward()
 
     def stop(self):
         print 'motor_stop'
@@ -178,3 +177,39 @@ class Cars:
         GPIO.output(self.LED1, True)  # LED1灭
         GPIO.output(self.LED2, True)  # LED2亮
 
+    def process_single_frame(self):
+        # print "reading image..."
+        # ret, img_frame = self.cap.read()
+        # print "reading image done"
+
+        # img_frame = cv2.resize(img_frame, (self.model.IMAGE_SIZE, self.model.IMAGE_SIZE))
+
+        # print "predicting image..."
+        # action_opt = np.argmax(self.model.predict(img_frame))
+        # print "predict done"
+
+        action_opt = raw_input('Choose a action(w,s,a,d): ')
+        print "predict done act=" + str(action_opt)
+
+        if action_opt == 'w':
+            self.Motor_Forward()
+            print "forward"
+        elif action_opt == 'a':
+            self.Motor_TurnLeft()
+            print "left"
+            time.sleep(0.01)
+        elif action_opt == 'd':
+            self.Motor_TurnRight()
+            time.sleep(0.01)
+            print "right"
+        else:
+            self.Motor_Stop()
+        pass
+
+    def run(self):
+        while True:
+            self.process_single_frame()
+
+if __name__ == '__main__':
+    demo = Cars()
+    demo.run()
